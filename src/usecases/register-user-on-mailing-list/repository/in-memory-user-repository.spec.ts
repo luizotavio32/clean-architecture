ce9676cb89d80ea1aa2e457a8265063eed3a5b2c
luizotavio32 <luizotavioindiana@hotmail.com>
2020-12-02 20:32:17 -0300

    test: return all users in the repository

--- src/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.spec.ts
+++ src/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.spec.ts
@@ -18,4 +18,11 @@ describe('In memory User repository', () => {
     const user = await userRepo.findUserByEmail('any@mail.com')
     expect(user.name).toBe('any_name')
   })
+
+  test('should return all users if it is found in the repository', async () => {
+    const users: UserData[] = [{ name: 'any_name', mail: 'any@mail.com' }, { name: 'second_name', mail: 'second@mail.com' }]
+    const userRepo = new InMemoryUserRepository(users)
+    const returnedUsers = userRepo.findAllUsers()
+    expect((await returnedUsers).length).toBe(2)
+  })
 })
