03d6e2971866ab1cce291dbd6995d3926764f533
luizotavio32 <luizotavioindiana@hotmail.com>
2020-12-09 19:41:30 -0300

    test: validate additional invalid emails

--- src/entities/email-validation.spec.ts
+++ src/entities/email-validation.spec.ts
@@ -50,4 +50,19 @@ describe('Email valdation', () => {
     const email = 'any email@mail.com'
     expect(Email.validate(email)).toBeFalsy()
   })
+
+  test('should not accept local part with two dots', () => {
+    const email = 'any..email@mail.com'
+    expect(Email.validate(email)).toBeFalsy()
+  })
+
+  test('should not accept local part with ending dot', () => {
+    const email = 'any.@mail.com'
+    expect(Email.validate(email)).toBeFalsy()
+  })
+
+  test('should not accept email without @ sign', () => {
+    const email = 'anymail.com'
+    expect(Email.validate(email)).toBeFalsy()
+  })
 })
